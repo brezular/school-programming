a31df997c4514311d09c0eb22aa77c8e5cfea48f
brezular <chicany@gmail.com>
2025-09-25 11:46:13 +0200

    Add files via upload

---
+++ Text-analysis/url.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+def read_text(page):
+	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+	r = requests.get(page, headers=headers)
+	soup = BeautifulSoup(r.text, "html.parser")
+	return soup.get_text()
+
+
